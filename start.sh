564490872c412e07c92872b769e9af9a0a57d03f
Alexotieno1717 <alexotieno900@gmail.com>
2020-11-02 21:03:39 +0300

    remove allowed host

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 DB_NAME=ddln3ipg2rv46s
 DB_USER=mmqqzakssgoprt
 DB_PASSWORD=e453e54381c177fe5ca9c35b21c4d65608ddb4152d2f1af26c135d982979cb82
-ALLOWED_HOSTS=neighbourhood715.herokuapp.com
 DB_HOST=ec2-34-200-106-49.compute-1.amazonaws.com
 DB_PORT=5432
 DEBUG=True
